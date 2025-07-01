0a5122c9a47db938f03caddb64d56e79963475a8
Daksh Patel <dakshpatel1700@gmail.com>
2025-07-01 12:14:39 -0400

    Minor Change to UI for Stats Page

--- Researcher-Pro-Frontend/stats.js
+++ Researcher-Pro-Frontend/stats.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
 async function getStats() {
     try{
 
+        showResult('Fetching Data ...');
+
         const response = await fetch('http://localhost:8080/api/research/getstats', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json'},
