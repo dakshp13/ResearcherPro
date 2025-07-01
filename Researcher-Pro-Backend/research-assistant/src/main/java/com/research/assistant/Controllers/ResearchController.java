94468a8bf5dce452269e13f4ee4c9dca2c529d56
Daksh Patel <dakshpatel1700@gmail.com>
2025-07-01 13:42:54 -0400

    Added A Rate Limiter using Bucket4j

--- Researcher-Pro-Backend/research-assistant/src/main/java/com/research/assistant/Controllers/ResearchController.java
+++ Researcher-Pro-Backend/research-assistant/src/main/java/com/research/assistant/Controllers/ResearchController.java
@@ -3,9 +3,15 @@ package com.research.assistant.Controllers;
 import com.research.assistant.Model.ResearchRequest;
 import com.research.assistant.Services.ResearchService;
 import com.research.assistant.Services.StatsService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/research")
@@ -15,16 +21,38 @@ public class ResearchController {
     private final ResearchService researchService;
     private final StatsService statsService;
 
+    //Adding a Rate Limiter
+    private final Bucket getStatsBucket = Bucket.builder()
+            .addLimit(Bandwidth.classic(1, Refill.intervally(1, Duration.ofMinutes(1))))
+            .build();
+
+    private final Bucket processBucket = Bucket.builder()
+            .addLimit(Bandwidth.classic(10, Refill.intervally(10, Duration.ofMinutes(1))))
+            .build();
+    //Adding a Rate Limiter
+
     @PostMapping("/process")
     public ResponseEntity<String> processContent(@RequestBody ResearchRequest request){
-        String result = researchService.processContent(request);
-        return ResponseEntity.ok(result);
+        if(processBucket.tryConsume(1)) {
+            String result = researchService.processContent(request);
+            return ResponseEntity.ok(result);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too many requests - please wait a minute before retrying.");
+        }
     }
 
     @GetMapping("/getstats")
     public ResponseEntity<String> getStats(){
-        String stats = statsService.getStats();
-        return ResponseEntity.ok(stats);
+        if(getStatsBucket.tryConsume(1)) {
+            String stats = statsService.getStats();
+            return ResponseEntity.ok(stats);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Too many requests - please wait a minute before retrying.");
+        }
     }
 
 }
